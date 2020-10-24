e9922f36876d5bc9ea4b8e76c526857007d5c1ea
Chuxi Wang <wangchuxi94@gmail.com>
2020-10-24 14:44:23 -0700

    added busted and isBlackJack method to the blackjackhand implementation

--- src/BlackJackHand.java
+++ src/BlackJackHand.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Disabled;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,7 +39,7 @@ public class BlackJackHand extends Hand{
             for (int i = 0; i < length; i++) {
                 int oldScore = scores.get(i);
                 scores.set(i, oldScore + toAdd[0]);
-                for (int j = i; j < toAdd.length; j++) {
+                for (int j = 1; j < toAdd.length; j++) {
                     scores.add(oldScore + toAdd[j]);
                 }
             }
@@ -46,10 +48,30 @@ public class BlackJackHand extends Hand{
     }
 
     private int[] getScores(Card card) {
-        if (card.value() != FaceValue.ACE) {
-            return new int[] {Math.min(card.value().getNumericValue(), 10)};
-        } else {
+        if (card.value() == FaceValue.ACE) {
             return new int[]{1, 11};
+        } else {
+            return new int[] {Math.min(card.value().getNumericValue(), 10)};
         }
+    }
+
+    public boolean busted() {
+        return score() > 21;
+    }
+
+    public boolean isBlackJack() {
+        if (cards.size() != 2) {
+            return false;
+        }
+        Card card1 = cards.get(0);
+        Card card2 = cards.get(1);
+
+        return (card1.value() == FaceValue.ACE && isFaceCard(card2))
+                || (card2.value() == FaceValue.ACE && isFaceCard(card1));
+
+    }
+
+    private static boolean isFaceCard(Card card) {
+        return card.value() == FaceValue.JACK || card.value() == FaceValue.QUEEN || card.value() == FaceValue.KING;
     }
 }
