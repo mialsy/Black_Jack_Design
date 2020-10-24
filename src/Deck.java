ba8d10666a3f20507ff9532c28f81ae15840c4e3
Chuxi Wang <wangchuxi94@gmail.com>
2020-10-24 12:07:05 -0700

    create card type and suit/facevalue enum for card

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Deck {
+    private static final Random random = new Random();
+
+    private final List<Card> cards = new ArrayList<>();
+    private int dealtIndex = 0;
+
+    public Deck() {
+        for (int i = 1; i <= 13; i++) {
+            for (Suit suit: Suit.values()) {
+                cards.add(new Card(,suit));
+            }
+        }
+    }
 }
